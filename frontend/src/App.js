548611d260b4762b8665f202ca500c014294715b
lunadiotic <ahmadhakim404@gmail.com>
2021-06-10 14:16:42 +0700

    create header and footer components

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,16 @@
 import './App.css'
+import Header from './components/Header'
+import Footer from './components/Footer'
 
 function App() {
   return (
-    <div className='App'>
-      <h1>Hello, react</h1>
-    </div>
+    <>
+      <Header></Header>
+      <main>
+        <h1>welcome to idshop</h1>
+      </main>
+      <Footer></Footer>
+    </>
   )
 }
 
