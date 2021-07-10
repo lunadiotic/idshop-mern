3f281f8201acb1085e3a0e5af451ff7010f19507
Lionatic <ahmadhakim404@gmail.com>
2021-07-10 11:09:11 +0700

    make simple api endpoint products

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,21 @@
 import express from 'express'
 import dotenv from 'dotenv'
+import products from './data/products.js'
 
 dotenv.config()
 const app = express()
 
 app.get('/', (req, res) => {
   res.json({ message: 'server is running' })
+})
+
+app.get('/api/products', (req, res) => {
+  res.json(products)
+})
+
+app.get('/api/products/:id', (req, res) => {
+  const product = products.find((product) => product._id == req.params.id)
+  res.json(product)
 })
 
 const PORT = process.env.APP_PORT || 5000
